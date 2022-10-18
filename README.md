af089eab71ef488dcdcda8215bd27b8f0fab1d9a
Abdourahmane Diop <cabraldiop18@gmail.com>
2022-10-18 09:48:11 +0000

    Build : Changer architecture for laravel boilerplate

--- README.md
+++ README.md
@@ -4,6 +4,15 @@
 Bureau officiel de Amadou Mansour Faye
 
 ## Fiche technique
--   php 8.0
--   Laravl 9.19
+-   Laravel boiler-plate
+-   "php": "^7.4|^8.0",
+-   "laravel/framework": "^8.54
 -   Mysql 8.0
+
+### Demo Credentials
+
+**Admin:** admin@admin.com  
+**Password:** secret
+
+**User:** user@user.com  
+**Password:** secret
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,28 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LocaleController;
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+ * Global Routes
+ *
+ * Routes that are used between both frontend and backend.
+ */
 
-Route::get('/', function () {
-    return view('welcome');
+// Switch between the included languages
+Route::get('lang/{lang}', [LocaleController::class, 'change'])->name('locale.change');
+
+/*
+ * Frontend Routes
+ */
+Route::group(['as' => 'frontend.'], function () {
+    includeRouteFiles(__DIR__.'/frontend/');
+});
+
+/*
+ * Backend Routes
+ *
+ * These routes can only be accessed by users with type `admin`
+ */
+Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'admin'], function () {
+    includeRouteFiles(__DIR__.'/backend/');
 });
